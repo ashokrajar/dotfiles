55bb85f4cc03dd044f8f2e878a72da102a41ead7
Ashok Raja <ashokraja.r@gmail.com>
2022-03-04 16:03:03 +0000

    updating shell prompt

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -849,9 +849,9 @@
   # Context color when running with privileges.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=160
   # Context color in SSH without privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=180
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=5
   # Default context color (no privileges, no SSH).
-  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=172
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=5
 
   # Context format when running with privileges: bold user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%B%n@%m'
